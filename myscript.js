69bb083b5ee99e999c27af01adc3b9a57d2be33b
masaun <unomasanori@unomasanori-no-MacBook-Air.local>
2016-10-19 11:38:05 +0900

    YouTube検索実装　https://developers.google.com/youtube/2.0/developers_guide_protocol_api_query_parameters

--- myscript.js
+++ myscript.js
@@ -1,15 +1,16 @@
 var mainCtrl = function($scope, $http) {
   $scope.doSearch = function() {
-    var url = 'https://www.googleapis.com/youtube/v3/videos?'
+    var url = 'https://gdata.youtube.com/feeds/api/videos?'
         + [
           'q=' + encodeURIComponent($scope.query),
-          'alt=json'
-          'max-results=10'
-          'v=2'
-          'callback=JSON_CALLBACK'
+          'alt=json',
+          'max-results=10',
+          'v=2',
+          'callback=JSON_CALLBACK',
         ].join('&');
     $http.jsonp(url).success(function(data) {
-      console.dir(data);
+      // console.dir(data);
+      $scope.results = data.feed.entry;
     });
-  });
+  }
 }
