d782007758c6f368d8672819e63fcb9dfe28c398
masaun <unomasanori@unomasanori-no-MacBook-Air.local>
2016-10-19 11:04:54 +0900

    YouTube APIの取得 https://developers.google.com/youtube/v3/getting-started

--- myscript.js
+++ myscript.js
@@ -1,3 +1,15 @@
 var mainCtrl = function($scope, $http) {
-  
+  $scope.doSearch = function() {
+    var url = 'https://www.googleapis.com/youtube/v3/videos?'
+        + [
+          'q=' + encodeURIComponent($scope.query),
+          'alt=json'
+          'max-results=10'
+          'v=2'
+          'callback=JSON_CALLBACK'
+        ].join('&');
+    $http.jsonp(url).success(function(data) {
+      console.dir(data);
+    });
+  });
 }
